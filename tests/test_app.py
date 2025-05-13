2b9d6f28e0f3f20ee85d579682e8b038a3a1daa0
shinjansarkar <shinjansarkar268@gmail.com>
2025-05-14 02:02:46 +0530

    Fix import error for GitLab CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
 # tests/test_app.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app, socketio
 
 import pytest
 from app import app, socketio
